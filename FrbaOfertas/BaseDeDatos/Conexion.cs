9758cd7c8790d387494b85cc64861d69968e8cef
IvanCasanova98 <38133709+IvanCasanova98@users.noreply.github.com>
2019-10-17 04:01:50 -0300

    Agrego Archivo configuracion para fecha y db

---
+++ FrbaOfertas/BaseDeDatos/Conexion.cs
@@ -0,0 +1,50 @@
+﻿using FrbaOfertas.PropiedadesConfig;
+using System.Data.SqlClient;
+using System;
+
+namespace FrbaOfertas.BaseDeDatos
+{
+    class Conexion
+    {
+        public static string getStringConnection()
+        {
+            return Config.Default.connectionString;
+        }
+
+        private static SqlConnection create()
+        {
+            return new SqlConnection(getStringConnection());
+        }
+
+        private static SqlConnection open(SqlConnection conn)
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return conn;
+        }
+
+        public static void close(SqlConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static SqlConnection getConnection()
+        {
+            return open(create());
+        }
+    }
+}
